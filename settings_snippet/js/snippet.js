271a22850bc19e319a660d48ee7385d9ffa75796
zivce <zivkovicmilos5@hotmail.com>
2017-11-28 15:58:51 +0100

    settings button with list added #1

---
+++ settings_snippet/js/snippet.js
@@ -0,0 +1,40 @@
+window.onload = function(){
+
+    let algsList = document.getElementById('list_of_algs');
+    let settings = document.getElementById('button_settings');
+
+    let showedAlgsList = false;
+
+    let listItems = algsList.querySelectorAll("li");
+
+
+    listItems.forEach((el) => {
+      el.onclick = function(){
+        let alreadyClicked = this.parentElement.querySelector('.clicked');
+        //skida sa prethodnog kliknutog
+        if(alreadyClicked)
+          alreadyClicked.className = "";
+        el.className = "";
+        el.className += "clicked";
+      }
+
+    })
+    settings.onclick =  function(){
+
+      if(!showedAlgsList){
+        algsList.style.display = "block";
+        algsList.className = "";
+        algsList.className += "opened";
+        showedAlgsList = true;
+      }
+      else {
+        algsList.style.display = "none";
+        algsList.className -= "opened";
+        showedAlgsList = false;
+      }
+    }
+
+
+
+
+}
