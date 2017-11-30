8d74dd0ab07ce81b454706bc09b5646e3c53b40a
zivce <zivkovicmilos5@hotmail.com>
2017-11-30 21:01:11 +0100

    snippet added with css,js,html ...

--- MSTmain.js
+++ MSTmain.js
@@ -1,8 +1,57 @@
-var myMST = new MSTCanvas();
-myMST.addListOfNodes(myNodes);
-myMST.addListOfEdges(myEdges);
-myMST.draw();
+window.onload = function(){
+
+
+/*settings snippet added */
+
+
+    let algsList = document.getElementById('list_of_algs');
+    let settings = document.getElementById('button_settings');
+
+    let showedAlgsList = false;
+
+    let listItems = algsList.querySelectorAll("li");
+
+
+    listItems.forEach((el) => {
+      el.onclick = function(){
+        let alreadyClicked = this.parentElement.querySelector('.clicked');
+        //skida sa prethodnog kliknutog
+
+        if(alreadyClicked)
+          alreadyClicked.className = "";
+
+        el.className = "";
+        el.className += "clicked";
+      }
+
+    })
+    settings.onclick =  function(){
+
+      if(!showedAlgsList){
+        algsList.style.display = "block";
+        algsList.className = "";
+        algsList.className += "opened";
+        showedAlgsList = true;
+      }
+      else {
+        algsList.style.display = "none";
+        algsList.className -= "opened";
+        showedAlgsList = false;
+      }
+    }
 
 
 
 
+
+/* settings end */
+
+
+
+  var myMST = new MSTCanvas();
+  myMST.addListOfNodes(myNodes);
+  myMST.addListOfEdges(myEdges);
+  myMST.draw();
+
+
+}
